d1fafd5f17ea92232ea95ec9045a2c32a22f4959
doailabs <128745776+doailabs@users.noreply.github.com>
2023-05-29 12:09:25 +0200

    Update getAnalytics.js

--- getAnalytics.js
+++ getAnalytics.js
@@ -15,6 +15,53 @@ function getAnalytics(startDate, endDate) {
     .then((data) => {
       // Log the success message if the request was successful
       console.log(`postAnalyticsConversationsDetailsQuery success! data: ${JSON.stringify(data, null, 2)}`);
+
+      // Initialize the tables
+      let conversationsTable = [];
+      let participantsTable = [];
+      let sessionsTable = [];
+      let segmentsTable = [];
+      let metricsTable = [];
+
+      // Process the data for each conversation
+      for (let conversation of data.conversationsDetails) {
+        // Add the conversation data to the conversations table
+        conversationsTable.push(conversation.conversation);
+
+        // Process the data for each participant in the conversation
+        for (let participant of conversation.participants) {
+          // Add the participant data to the participants table
+          participantsTable.push(participant);
+
+          // Process the data for each session in the participant
+          for (let session of participant.sessions) {
+            // Add the session data to the sessions table
+            sessionsTable.push(session);
+
+            // Process the data for each segment in the session
+            for (let segment of session.segments) {
+              // Add the segment data to the segments table
+              segmentsTable.push(segment);
+            }
+          }
+
+          // Process the data for each metric in the participant
+          for (let metric of participant.metrics) {
+            // Add the metric data to the metrics table
+            metricsTable.push(metric);
+          }
+        }
+      }
+
+      // Return the tables
+      return {
+        'conversations': conversationsTable,
+        'participants': participantsTable,
+        'sessions': sessionsTable,
+        'segments': segmentsTable,
+        'metrics': metricsTable
+      };
+
     })
     .catch((err) => {
       // Log the error message if the request failed
